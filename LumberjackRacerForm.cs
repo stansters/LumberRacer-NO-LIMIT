4f81f5e87edbdf625fb7e4508aed4ede35abc3bc
stansters <91402728+stansters@users.noreply.github.com>
2024-10-15 22:58:43 +0000

    Removed limit

--- LumberjackRacerForm.cs
+++ LumberjackRacerForm.cs
@@ -62,7 +62,6 @@ namespace LumberRacer
 
                 var textbox = new NumericUpDown
                 {
-                    Maximum = displayAttribute?.Max ?? 400,
                     Value = displayAttribute?.DefaultValue ?? 0
                 };
 
